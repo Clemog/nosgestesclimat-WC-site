1bbf111578e7d6fde8230cd159585f5c0a1b90f6
Clement AUGER <clem.auger@hotmail.fr>
2022-03-15 14:29:15 +0100

    Ajout d'un lien vers le guide dans la salle

--- source/sites/publicodes/conference/Instructions.tsx
+++ source/sites/publicodes/conference/Instructions.tsx
@@ -34,7 +34,6 @@ export default ({
 	const URLbase = `https://${window.location.hostname}`
 	const URLPath = `/${mode}/${room || newRoom}`,
 		shareURL = URLbase + URLPath
-
 	return (
 		<div>
 			{!room && (
@@ -212,6 +211,20 @@ export default ({
 			{newRoom !== '' && !room && (
 				<InstructionBlock index="6" title="Prêt à démarrer ?">
 					<LoadingButton {...{ mode, URLPath, room: room || newRoom }} />
+				</InstructionBlock>
+			)}
+			{room && (
+				<InstructionBlock
+					noIndex={started}
+					title={
+						<span>
+							{emoji('📊')} Analysez les résultats et animez les discussions !
+						</span>
+					}
+				>
+					Les résultats sont là, que faire ? Notre guide vous accompagne dans
+					vos réflexions et vos discussions !{' '}
+					<Link to={URLGuide}>{URLGuide}</Link>
 				</InstructionBlock>
 			)}
 		</div>
