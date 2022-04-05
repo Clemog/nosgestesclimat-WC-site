deb33ea0ca4fb3fc3f36d7fdaa6125b3722a81f9
Mael <mael.thomas@data.gouv.fr>
2022-04-05 12:07:33 +0200

    :sparkles: Renommage du composant CarbonImpact

--- source/sites/publicodes/ScoreBar.tsx
+++ source/sites/publicodes/ScoreBar.tsx
@@ -54,6 +54,7 @@ export default ({ }) => {
 			css={`
 				${!demoMode &&
 				`
+				margin-bottom: 1.2rem;
 				@media (max-width: 800px) {
 					margin: 0;
 					
@@ -63,6 +64,7 @@ export default ({ }) => {
 					z-index: 10;
 					width: 100%;
 				}`}
+
 				color: var(--textColor);
 				a {
 					color: inherit;
