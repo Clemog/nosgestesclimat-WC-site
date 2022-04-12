705fbe2dd54ec3699ac36e20af6dc2248efb76aa
Mael <mael.thomas@data.gouv.fr>
2022-04-12 10:20:23 +0200

    Apparition retardée de la FAQ
    Pour que les nouveaux utilisateurs se concentre sur les déjà nombreuses
    options d'intéraction, et trouvent plus facilement le début du test

--- source/sites/publicodes/BandeauContribuer.js
+++ source/sites/publicodes/BandeauContribuer.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import emoji from 'react-easy-emoji'
 import { Link } from 'react-router-dom'
+import animate from 'Components/ui/animate'
 
 export default () => {
+	const [visible, setVisible] = useState(false)
+	useEffect(() => {
+		setTimeout(() => setVisible(true), 5000)
+	}, [])
+	if (!visible) return null
 	return (
 		<div css=" text-align: center; color: black; margin: .6rem 0">
 			Une question, un problème ? {emoji('📮')}{' '}
