4b1152b00bd4ffc3d78fec9d6577661a10875f8e
Clement AUGER <clem.auger@hotmail.fr>
2022-05-04 17:06:29 +0200

    Amélioration titres des pages en ajoutant une balise Meta

--- source/sites/publicodes/Diffuser.js
+++ source/sites/publicodes/Diffuser.js
@@ -7,7 +7,7 @@ import Meta from '../../components/utils/Meta'
 export default () => (
 	<section className="ui__ container" id="about">
 		<Meta
-			title="À propos de Nos Gestes Climat"
+			title="Diffuser Nos Gestes Climat"
 			description={`
 			Ce simulateur vous permet d'évaluer votre empreinte carbone individuelle professionnelle (liée à vos activités professionnelles) et par grandes catégories (alimentation, déplacements domicile-travail, déplacements professionnels, véhicules de fonction, numérique) et surtout de passer à l’action. Nous sommes conscients que le changement de pratiques et en particulier en entreprise prend du temps. Nous avons fait le choix d’inclure des pistes d’action de réduction des émissions de gaz à effet de serre à l’échelle de l’entreprise car nous sommes convaincus que ces actions collectives sont nécessaires à la réduction des émissions de gaz à effet de serre. Les actions individuelles et collectives proposées apparaissent en fonction de vos réponses.
 
