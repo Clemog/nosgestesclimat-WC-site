d0dadb01571c32509899444c5e11b2957eae170e
Mael <mael.thomas@data.gouv.fr>
2022-08-24 13:37:36 +0200

    Plus besoin d'inverser la couleur de l'icône profil sur l'accueil

--- source/sites/publicodes/Landing.js
+++ source/sites/publicodes/Landing.js
@@ -148,7 +148,7 @@ const ProfileLink = () => {
 					<img
 						aria-hidden="true"
 						src={openmojiURL('profile')}
-						css="width: 2rem; filter: invert(1)"
+						css="width: 2rem"
 					/>
 				</Link>
 			</div>
