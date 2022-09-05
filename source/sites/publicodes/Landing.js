9bbcccbce3577ff0174beb502a9685637a54ccf9
Clément <55186402+Clemog@users.noreply.github.com>
2022-09-05 17:46:52 +0200

    Revert "Ajout enquête utilisateurs"

--- source/sites/publicodes/Landing.js
+++ source/sites/publicodes/Landing.js
@@ -11,7 +11,6 @@ import { TrackerContext } from '../../components/utils/withTracker'
 import DocumentationButton from './DocumentationButton'
 import Illustration from 'Images/ecolab-climat-dessin.svg'
 import { useProfileData } from './Profil'
-import SurveyModal from './SurveyModal'
 
 const SurveyModal = React.lazy(() => import('./SurveyModal'))
 
