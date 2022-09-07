f92e397f99a347af33d645f3ea31b6ef8956c39f
Mael <mael.thomas@data.gouv.fr>
2022-09-07 17:25:12 +0200

    On ne parse pas les règles tant que c'est pas nécessaire
    Ce sont les composants via WithEngine qui le font

--- source/reducers/rootReducer.ts
+++ source/reducers/rootReducer.ts
@@ -233,6 +233,16 @@ function thenRedirectTo(state = null, { type, to }) {
 		return to
 	} else return state
 }
+type EngineState = 'requested' | 'ready'
+type EngineAction = {
+	type: string
+	to: EngineState
+}
+function engineState(state = null, { type, to }: EngineAction) {
+	if (type === 'SET_ENGINE') {
+		return to
+	} else return state
+}
 
 const defaultToNull = (arg) => arg ?? null
 
