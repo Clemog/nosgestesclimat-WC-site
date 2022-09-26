cd40fdc00d973163d23a9fc6dd3371094d12c6e7
Clement AUGER <clem.auger@hotmail.fr>
2022-09-26 20:29:39 +0200

    enquête utilisateur septembre 2022 terminée

--- source/sites/publicodes/Landing.js
+++ source/sites/publicodes/Landing.js
@@ -1,7 +1,7 @@
 import Illustration from 'Components/AnimatedIllustration'
 import animate from 'Components/ui/animate'
 import LogoADEME from 'Images/logoADEME.svg'
-import React, { Suspense, useContext, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import emoji from 'react-easy-emoji'
 import { Link } from 'react-router-dom'
 import NewsBanner from '../../components/NewsBanner'
@@ -15,13 +15,10 @@ import LandingContent from './LandingContent'
 import LandingExplanations from './LandingExplanations'
 import { useProfileData } from './Profil'
 
-const SurveyModal = React.lazy(() => import('./SurveyModal'))
-
 const fluidLayoutMinWidth = '1200px'
 
 export default () => {
 	const tracker = useContext(TrackerContext)
-	const [showSurveyModal, setShowSurveyModal] = useState(false)
 	const mobile = useMediaQuery(`(max-width: ${fluidLayoutMinWidth})`)
 	const { isIframe } = useContext(IframeOptionsContext)
 
