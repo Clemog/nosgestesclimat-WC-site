26dd837834a73bab8def7dc271255e15310ea387
Emile Rolley <emile.rolley@tuta.io>
2022-10-03 14:58:30 +0200

    fix(ui): no overlapping btw the weight result and the ?

--- source/sites/publicodes/ScoreBar.tsx
+++ source/sites/publicodes/ScoreBar.tsx
@@ -112,14 +112,10 @@ export default ({ }) => {
 								alt="Planète représentant le changement climatique"
 							/>
 							{!actionMode ? (
-								<div css="width: 8rem">
-									<HumanWeight
-										nodeValue={nodeValue}
-										overrideValue={
-											actionMode && actionTotal !== 0 && actionTotal
-										}
-									/>
-								</div>
+								<HumanWeight
+									nodeValue={nodeValue}
+									overrideValue={actionMode && actionTotal !== 0 && actionTotal}
+								/>
 							) : (
 								<DiffHumanWeight
 									{...{ nodeValue, engine, rules, actionChoices }}
