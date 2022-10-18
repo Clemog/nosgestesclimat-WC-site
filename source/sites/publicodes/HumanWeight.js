c0ab4197eed87ec5cf7e27401780b4d24d0c4c98
Mael <mael.thomas@data.gouv.fr>
2022-10-18 08:58:33 +0200

    🇬🇧incorrect translation of the CO2e unit suffix

--- source/sites/publicodes/HumanWeight.js
+++ source/sites/publicodes/HumanWeight.js
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
 import { correctValue } from '../../components/publicodesUtils'
 import { getLangFromAbreviation, getLangInfos } from '../../locales/translation'
@@ -41,7 +41,11 @@ const HumanWeight = ({
 	const currentLangInfos = getLangInfos(getLangFromAbreviation(i18n.language))
 
 	if (!unitSuffix) {
-		unitSuffix = t('de') + ' CO₂-e / ' + t('an')
+		unitSuffix = (
+			<span>
+				<Trans i18nKey="humanWeight.unitSuffix" /> CO₂-e / {t('an')}
+			</span>
+		)
 	}
 
 	const [value, unit] =
